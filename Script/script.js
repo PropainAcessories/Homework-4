f0ebcefdad276db5d3e15c84830b706151680dc2
HankHill <hhc3h8@gmail.com>
2022-07-25 07:23:05 -0500

    query selectors and the question array

---
+++ Script/script.js
@@ -0,0 +1,21 @@
+// Query Selectors.
+var hiscore = document.querySelector("hiscore");
+var quizbutton = document.querySelectorAll(".btn");
+var a = document.querySelector("#one");
+var a = document.querySelector("#two");
+var a = document.querySelector("#three");
+var a = document.querySelector("#four");
+
+var questions = [
+"What does DOM stand for?",
+"What is JavaScript usually used for in web development?",
+"What is the difference between querySelector and querySelectorAll?",
+"What is the difference between a method and a function?",
+"What is the purpose of an iteration?",
+"What type of statement begins with if?",
+"What does the push method do?",
+"What are the three types of variable?",
+"Which of the following correctly defines boolean?",
+"Are Java and JavaScript related?",
+]
+
