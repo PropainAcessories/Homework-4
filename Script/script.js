a1f5a098f689392479aed31ee8f3a6b410db1e07
HankHill <hhc3h8@gmail.com>
2022-07-26 16:05:11 -0500

    finished some functions; will touch up to appropriately hide HTML elements and make quiz run smoother

--- Script/script.js
+++ Script/script.js
@@ -3,10 +3,10 @@ var questions = [
     {
         question: "What does DOM stand for?",    
         answer: [
-            {a: "Document Object Model", id: "a"}, 
-            {b: "Direct Ordinance Machine", id: "b"},
-            {c: "Document Orientation Management", id: "c"}, 
-            {d: "Document Object Machine", id: "d"},
+            {choice: "Document Object Model", id: "a"}, 
+            {choice: "Direct Ordinance Machine", id: "b"},
+            {choice: "Document Orientation Management", id: "c"}, 
+            {choice: "Document Object Machine", id: "d"},
         ],
         hit: "a",
     },
@@ -14,10 +14,10 @@ var questions = [
     {
         question: "What is JavaScript usually used for in web development?",   
         answer: [
-            {a: "Cybersecurity", id: "a"}, 
-            {b: "Web Marketing", id: "b"},
-            {c: "Military Applications", id: "c"}, 
-            {d: "Creating interactive and dynamic web apps", id: "d"},
+            {choice: "Cybersecurity", id: "a"}, 
+            {choice: "Web Marketing", id: "b"},
+            {choice: "Military Applications", id: "c"}, 
+            {choice: "Creating interactive and dynamic web apps", id: "d"},
             ],
             hit: "d",
     },
@@ -25,10 +25,10 @@ var questions = [
     {
         question: "What is the difference between querySelector and querySelectorAll?",
         answer: [
-            {a: "One selects a given name; the other selects all within the parent", id: "a"},
-            {b: "There is no difference", id: "b"},
-            {c: "querySelectorAll selects the entire page", id: "c"},
-            {d: "None of the above", id: "d"},
+            {choice: "One selects a given name; the other selects all within the parent", id: "a"},
+            {choice: "There is no difference", id: "b"},
+            {choice: "querySelectorAll selects the entire page", id: "c"},
+            {choice: "None of the above", id: "d"},
             ],
             hit: "a",
     },
@@ -36,10 +36,10 @@ var questions = [
     {
         question: "What is the difference between a method and a function?",
         answer: [
-            {a: "A method is pre-defined, you must define a function yourself", id: "a"},
-            {b: "Method man is a rapper dude", id: "b"},
-            {c: "You can function but you can't method", id: "c"},
-            {d: "None of the above", id: "d"},
+            {choice: "A method is pre-defined, you must define a function yourself", id: "a"},
+            {choice: "Method man is a rapper dude", id: "b"},
+            {choice: "You can function but you can't method", id: "c"},
+            {choice: "None of the above", id: "d"},
         ],
         hit: "a",
     },
@@ -47,10 +47,10 @@ var questions = [
     {
         question: "What is the purpose of an iteration?",
         answer: [
-            {a: "To re-tell a story in a different time i.e: Comic books", id: "a"},
-            {b: "Marketing gimmick", id: "b"},
-            {c: "To destabilize the global economy", id: "c"},
-            {d: "To get code to execute a certain number of times or until conditions are met", id: "d"},
+            {choice: "To re-tell a story in a different time i.e: Comic books", id: "a"},
+            {choice: "Marketing gimmick", id: "b"},
+            {choice: "To destabilize the global economy", id: "c"},
+            {choice: "To get code to execute a certain number of times or until conditions are met", id: "d"},
             ],
             hit: "d",
     },
@@ -58,10 +58,10 @@ var questions = [
     {
         question: "What type of statement begins with if?",
         answer: [
-            {a: "A question isn't a statement dummy", id: "a"},
-            {b: "Ifs begin questions", id: "b"},
-            {c: "A conditional statement", id: "c"},
-            {d: "A fashion statement", id: "d"},
+            {choice: "A question isn't a statement dummy", id: "a"},
+            {choice: "Ifs begin questions", id: "b"},
+            {choice: "A conditional statement", id: "c"},
+            {choice: "A fashion statement", id: "d"},
             ],
             hit: "c",
     },
@@ -69,10 +69,10 @@ var questions = [
     {
         question: "What does the push method do?",
         answer: [
-            {a: "Puts items to an object or array", id: "a"},
-            {b: "Pushes weaker code out of the way", id: "b"},
-            {c: "Git pushes so you don't have to", id: "c"},
-            {d: "All of the above", id: "d"},
+            {choice: "Puts items to an object or array", id: "a"},
+            {choice: "Pushes weaker code out of the way", id: "b"},
+            {choice: "Git pushes so you don't have to", id: "c"},
+            {choice: "All of the above", id: "d"},
             ],
             hit: "a",
     },
@@ -80,10 +80,10 @@ var questions = [
     {
         question: "What are the three types of variable?",
         answer: [
-            {a: "Larry, Curly, and Moe", id: "a"},
-            {b: "Letter, Number, and Symbol", id: "b"},
-            {c: "String, Boolean, and Number", id: "c"},
-            {d: "None of the above.", id: "d"},
+            {choice: "Larry, Curly, and Moe", id: "a"},
+            {choice: "Letter, Number, and Symbol", id: "b"},
+            {choice: "String, Boolean, and Number", id: "c"},
+            {choice: "None of the above.", id: "d"},
             ],
             hit: "c",
     },
@@ -91,10 +91,10 @@ var questions = [
     {
         question: "Which of the following correctly defines boolean?",
         answer: [
-            {a: "A spicy soup from Lousiana", id: "a"},
-            {b: "A spicy soup from the Middle-East", id: "b"},
-            {c: "That guy that always sits outside of the gas station", id: "c"},
-            {d: "A variable set to true or false", id: "d"},
+            {choice: "A spicy soup from Lousiana", id: "a"},
+            {choice: "A spicy soup from the Middle-East", id: "b"},
+            {choice: "That guy that always sits outside of the gas station", id: "c"},
+            {choice: "A variable set to true or false", id: "d"},
             ],
             hit: "d",
     },
@@ -102,10 +102,10 @@ var questions = [
     {
         question: "Are Java and JavaScript related?",
         answer: [
-            {a: "Yes by blood", id: "a"},
-            {b: "Yes Same inventor", id: "b"},
-            {c: "No", id: "c"},
-            {d: "Yes, but only by marriage", id: "d"},
+            {choice: "Yes by blood", id: "a"},
+            {choice: "Yes Same inventor", id: "b"},
+            {choice: "No", id: "c"},
+            {choice: "Yes, but only by marriage", id: "d"},
             ],
             hit: "c",
     },
@@ -132,20 +132,34 @@ var inQuiz = false;
 
 // Variable for any button to start the quiz at the beginning.
 var quizbtn = document.querySelector("#start");
+// Selects the main section of the page
+var bigBox = document.querySelector("#bigbox");
+// To access the question section
+var question1 = document.querySelector("#question");
 // To put in the questions
-var questionEl = document.querySelector("#quiz", "h2");
+var questionEl = document.querySelector(".question");
 // For the answers
-var answerEl = document.querySelector(".btn");
-// Time keeping section
-var time = document.querySelector("#time");
+var answerEl = document.querySelector(".answers");
+// For Hits
+var hits = document.querySelector(".hit");
 // Buttons for the answers
 var mkBtn = document.createElement("button");
-// High scores
+// For the done page
+var finished = document.querySelector("#finshed");
+// Defines the score
+var userScore = document.querySelector("#userScore");
+// For the submit button
+var submitBtn = document.querySelector("#submit");
+// Time keeping section
+var time = document.querySelector("#time");
+// for their initials
+var initials = document.querySelector("#inputform");
+// goes back to the main page
+var goback = document.querySelector("#goBack");
+// clears the highscores
+var cleanScores = document.querySelector("#clear");
+// High score button
 var hiscore = document.querySelector("#hiscore");
-// Quiz Reset
-var reset = document.querySelector("#reset");
-
-
 // quiz starts here.
 
 function startQuiz() {
@@ -156,8 +170,14 @@ function startQuiz() {
     timer = 120;
     hiscore.setAttribute("disabled", "true");
 
-
     quizTimer();
+
+    bigBox.classList.remove("beercan");
+    bigBox.classList.add("bounce");
+
+    bigBox.classList.add("beercan");
+    bigBox.classList.remove("bounce");
+    nextpage();
 
 };
 
@@ -165,11 +185,40 @@ function startQuiz() {
 function clearPage () {
     questionEl.textContent = "";
     answerEl.textContent = "";
+    hits.textContent = "";
 
+    if (showScore) {
+        // If quizArray >= 0 {
+        bigBox.classList.remove("beercan");
+        bigBox.classList.add("bounce");
+        finished.classList.remove("beercan");
+        finished.classList.add("bounce");
+    } else {
+        return;
+    }
+    //}
 };
 
 // Brings on the next question; or the end of the quiz.
 function nextpage() {
+    if (!inQuiz || quizArray === questions.length) return;
+    // Puts a margin on the bottom of the questions.
+    questionEl.classList.add("mb-5");
+    questionEl.innerHTML = questions[quizArray].question;
+
+    for (let i=0; i < questions[quizArray].answer.length; i++) {
+        var mkBtn = document.createElement("button");
+        mkBtn.classList.add("btn");
+        mkBtn.textContent = questions[quizArray].answer[i].choice;
+        answerEl.classList.add("beercan");
+        answerEl.appendChild("mkBtn");
+        if (questions[quizArray].answer[i].id === questions[quizArray].hit) {
+            mkBtn.addEventListener("click", ansCorrect);
+        } else {
+            mkBtn.addEventListener("click", ansWrong);
+        }
+
+    }
 
 };
 
@@ -178,8 +227,7 @@ function ansCorrect(){
     score++;
     quizArray++;
     clearPage();
-    //placeholder for code to be written
-    //placeholder for code to be written
+    hits.textContent = "CORRECT"
     nextpage();
 
 }
@@ -189,8 +237,7 @@ function ansWrong(){
     timer = timer - 10;
     quizArray++;
     clearPage();
-    //placeholder
-    //placeholder
+    hits.textContent = "WRONG"
     nextpage();
 }
    
@@ -212,9 +259,17 @@ function stopQuiz() {
     timer = 0;
     inQuiz = false;
     clearPage();
+    hiscore.removeAttribute("disabled");
+    finished.classList.remove("bounce");
+    finished.classList.add("beercan");
+    userScore.textContent = points;
+
+    if (initials.value !== "") {
+        initials.value = "";
+    }
 };
 
-function scoreMgr(){
+function scoreMgr() {
 
 }
 
