2f1cdc9b35329b24a759ab697cce86b899ef6bf6
HankHill <hhc3h8@gmail.com>
2022-07-26 14:03:22 -0500

    re-added hit to objects; finished high score display markup.

--- Script/script.js
+++ Script/script.js
@@ -1,103 +1,113 @@
 // Array for the questions and answers.
 var questions = [
     {
-        question1: "What does DOM stand for?",    
-        answer1: [
+        question: "What does DOM stand for?",    
+        answer: [
             {a: "Document Object Model", id: "a"}, 
             {b: "Direct Ordinance Machine", id: "b"},
             {c: "Document Orientation Management", id: "c"}, 
             {d: "Document Object Machine", id: "d"},
         ],
+        hit: "a",
     },
     
     {
-        question2: "What is JavaScript usually used for in web development?",   
-        answer2: [
+        question: "What is JavaScript usually used for in web development?",   
+        answer: [
             {a: "Cybersecurity", id: "a"}, 
             {b: "Web Marketing", id: "b"},
             {c: "Military Applications", id: "c"}, 
             {d: "Creating interactive and dynamic web apps", id: "d"},
             ],
+            hit: "d",
     },
     
     {
-        question3: "What is the difference between querySelector and querySelectorAll?",
-        answer3: [
+        question: "What is the difference between querySelector and querySelectorAll?",
+        answer: [
             {a: "One selects a given name; the other selects all within the parent", id: "a"},
             {b: "There is no difference", id: "b"},
             {c: "querySelectorAll selects the entire page", id: "c"},
             {d: "None of the above", id: "d"},
             ],
+            hit: "a",
     },
    
     {
-        question4: "What is the difference between a method and a function?",
-        answer4: [
+        question: "What is the difference between a method and a function?",
+        answer: [
             {a: "A method is pre-defined, you must define a function yourself", id: "a"},
             {b: "Method man is a rapper dude", id: "b"},
             {c: "You can function but you can't method", id: "c"},
             {d: "None of the above", id: "d"},
         ],
+        hit: "a",
     },
     
     {
-        question5: "What is the purpose of an iteration?",
-        answer5: [
+        question: "What is the purpose of an iteration?",
+        answer: [
             {a: "To re-tell a story in a different time i.e: Comic books", id: "a"},
             {b: "Marketing gimmick", id: "b"},
             {c: "To destabilize the global economy", id: "c"},
             {d: "To get code to execute a certain number of times or until conditions are met", id: "d"},
             ],
+            hit: "d",
     },
     
     {
-        question6: "What type of statement begins with if?",
-        answer6: [
+        question: "What type of statement begins with if?",
+        answer: [
             {a: "A question isn't a statement dummy", id: "a"},
             {b: "Ifs begin questions", id: "b"},
             {c: "A conditional statement", id: "c"},
             {d: "A fashion statement", id: "d"},
             ],
+            hit: "c",
     },
    
     {
-        question7: "What does the push method do?",
-        answer7: [
+        question: "What does the push method do?",
+        answer: [
             {a: "Puts items to an object or array", id: "a"},
             {b: "Pushes weaker code out of the way", id: "b"},
             {c: "Git pushes so you don't have to", id: "c"},
             {d: "All of the above", id: "d"},
             ],
+            hit: "a",
     },
 
     {
-        question8: "What are the three types of variable?",
-        answer8: [
+        question: "What are the three types of variable?",
+        answer: [
             {a: "Larry, Curly, and Moe", id: "a"},
             {b: "Letter, Number, and Symbol", id: "b"},
             {c: "String, Boolean, and Number", id: "c"},
             {d: "None of the above.", id: "d"},
             ],
+            hit: "c",
     },
 
     {
-        question9: "Which of the following correctly defines boolean?",
-        answer9: [
+        question: "Which of the following correctly defines boolean?",
+        answer: [
             {a: "A spicy soup from Lousiana", id: "a"},
             {b: "A spicy soup from the Middle-East", id: "b"},
             {c: "That guy that always sits outside of the gas station", id: "c"},
             {d: "A variable set to true or false", id: "d"},
             ],
+            hit: "d",
     },
 
     {
-        question10: "Are Java and JavaScript related?",
-        answer10: [
+        question: "Are Java and JavaScript related?",
+        answer: [
             {a: "Yes by blood", id: "a"},
             {b: "Yes Same inventor", id: "b"},
             {c: "No", id: "c"},
             {d: "Yes, but only by marriage", id: "d"},
             ],
+            hit: "c",
     },
 ];
 
@@ -145,7 +155,7 @@ function startQuiz() {
     inQuiz = true;
     timer = 120;
     hiscore.setAttribute("disabled", "true");
-    reset.setAttribute("disabled", "true");
+
 
     quizTimer();
 
@@ -154,6 +164,8 @@ function startQuiz() {
 // Clears what is currently on the page
 function clearPage () {
     questionEl.textContent = "";
+    answerEl.textContent = "";
+
 };
 
 // Brings on the next question; or the end of the quiz.
